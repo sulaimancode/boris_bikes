2f641dfe49b63f519d710ee300de631503afd894
Slaiman Ahmadshah <sulaimanahmad321@gmail.com>
2017-04-11 19:06:47 +0100

    half way through step 12

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,21 @@
 require_relative 'bike'
+
 class DockingStation
+
+attr_reader :bike
+
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+    @bike = bike
+  end
+
 end
+
+# attr_reader :what_bike
+#
+# def initialize
+#   @what_bike = Bike.new
+# end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,16 +1,16 @@
 require 'docking_station'
 
 describe DockingStation do
-      it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to :release_bike }
 
-        it "Should create a bike" do
-          expect(subject.release_bike).to be_a Bike
-        end
+  it "Should create a bike" do
+    expect(subject.release_bike).to be_a Bike
+  end
 
-      it "Bike should be working" do
-         expect(subject.release_bike).to be_working
-       end
+  it "Bike should be working" do
+    expect(subject.release_bike).to be_working
+  end
 
+  it { is_expected.to respond_to :dock_bike }
 
-        # it  { is_expected.to respond_to :working? }
 end
